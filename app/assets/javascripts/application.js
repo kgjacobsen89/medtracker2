2f5e5837bca045aef01eba13a20d689a7bc262ed
kgjacobsen89 <kgjacobsen89@gmail.com>
2014-10-16 17:54:33 -0700

    reversed last commit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,10 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require bootstrap-sprockets
-//= require bootstrap
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap-sprockets
+//= require bootstrap
 //= require_tree .
 
 
