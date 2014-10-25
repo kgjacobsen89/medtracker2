5990e45042d441d4113bb0aea2f9fece4b875662
kgjacobsen89 <kgjacobsen89@gmail.com>
2014-10-24 17:48:49 -0700

    rescue not working

--- app/models/medication.rb
+++ app/models/medication.rb
@@ -2,12 +2,14 @@ class Medication < ActiveRecord::Base
 	before_save do  |medication| 
 		begin 
 			medication.name = medication.name.strip.downcase 
-		rescue "Name has already been taken"
+		rescue ActiveRecord::RecordNotUnique => error
+			 errors[:base] << error.message
+			# "don't try to save it..."
 			redirect_to new_patient_medication_path
 		end
 	end
 
-	validates_uniqueness_of :name
+	# validates_uniqueness_of :name
 
 	def cap_name
 		name.capitalize
